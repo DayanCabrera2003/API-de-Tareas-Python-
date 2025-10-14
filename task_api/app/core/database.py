f09277704eae77e534bd034394e77338c1a84890
DayanCabrera2003 <dayancorvo@gmail.com>
2025-10-14 18:08:21 -0400

    chore: add .env file with PostgreSQL configuration settings

--- task_api/app/core/database.py
+++ task_api/app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import Settings
 
-settings = Settings()
+settings = Settings() #type: ignore
 engine = create_engine(settings.DATABASE_URL, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
